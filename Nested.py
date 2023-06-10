2b301f17b455239872912789b262f8a04d93ba65
Laksh Singh <132688007+LakshSingh1@users.noreply.github.com>
2023-06-10 13:00:17 -0700

    Add files via upload

---
+++ Nested.py
@@ -0,0 +1,26 @@
+
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Bryant'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Andres', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 15, 'y': 30} ]
+
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+print((students))
+
+
+names = { "Michael","John", "Mark","KB" }
+for each_key in names:
+    print(each_key)
+
